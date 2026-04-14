f1a0384c29bd66fb4ea36b1e8585afbdd48006e3
marion909 <neuhausermario1111@gmail.com>
2026-04-14 16:24:01 +0200

    fix: withGradleWrapper now correctly patches gradle-wrapper.properties via withDangerousMod

--- plugins/withGradleWrapper.js
+++ plugins/withGradleWrapper.js
@@ -1,22 +1,31 @@
-const { withGradleProperties } = require('@expo/config-plugins');
+const { withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
 
 /**
- * Pins the Gradle distribution to a version known to be available on EAS build workers.
+ * Pins the Gradle wrapper distribution to a version pre-cached on EAS build workers.
+ * Must use withDangerousMod to edit gradle/wrapper/gradle-wrapper.properties,
+ * NOT withGradleProperties (which only edits android/gradle.properties).
  */
 module.exports = function withGradleWrapper(config, { gradleVersion = '8.10.2' } = {}) {
-  return withGradleProperties(config, (config) => {
-    const props = config.modResults;
+  return withDangerousMod(config, [
+    'android',
+    (config) => {
+      const wrapperPropsPath = path.join(
+        config.modRequest.platformProjectRoot,
+        'gradle', 'wrapper', 'gradle-wrapper.properties'
+      );
 
-    const urlKey = 'distributionUrl';
-    const idx = props.findIndex((p) => p.type === 'property' && p.key === urlKey);
-    const newUrl = `https\\://services.gradle.org/distributions/gradle-${gradleVersion}-bin.zip`;
+      if (fs.existsSync(wrapperPropsPath)) {
+        let contents = fs.readFileSync(wrapperPropsPath, 'utf8');
+        contents = contents.replace(
+          /^distributionUrl=.*$/m,
+          `distributionUrl=https\\://services.gradle.org/distributions/gradle-${gradleVersion}-bin.zip`
+        );
+        fs.writeFileSync(wrapperPropsPath, contents);
+      }
 
-    if (idx !== -1) {
-      props[idx].value = newUrl;
-    } else {
-      props.push({ type: 'property', key: urlKey, value: newUrl });
-    }
-
-    return config;
-  });
+      return config;
+    },
+  ]);
 };
